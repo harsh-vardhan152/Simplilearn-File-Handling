e57097e968390eb875e513201234db7b00289fae
Harsh Vardhan <54477764+harsh-vardhan152@users.noreply.github.com>
2022-07-22 17:54:34 +0530

    update code is written so that we can able to add

--- File Handling/src/handling/updateafile.java
+++ File Handling/src/handling/updateafile.java
@@ -4,23 +4,22 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class writeinafile {
-
+public class updateafile {
 	public static void main(String[] args) {
 		String Data;
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the text that you want to write in the text file");
+		System.out.println("Enter the text that you want to add in the text file");
 		Data = sc.nextLine();
 
 		try {
-			FileWriter Text = new FileWriter("java.txt");
+			FileWriter Text = new FileWriter("java.txt", true);
 			Text.write(Data);
 			System.out.println("Data is enter in the file successfully ");
 			Text.close();
 			
 		} catch (IOException e) {
-			System.out.println("File write error");
+			System.out.println("File update error");
 		}
 	}
 }
